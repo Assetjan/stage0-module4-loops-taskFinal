7a59d079d61f1334c7c08f7a07fed5540f2ef8e5
Asset <assetziyabek@gmail.com>
2023-02-02 22:01:31 +0600

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/MultiplicationTable.java
@@ -3,9 +3,9 @@ package school.mjc.stage0.loops.finalTask;
 public class MultiplicationTable {
     public void printTable(int numberTableToPrint){
         int k;
-        for (int i = 1; i<=9; i++){
+        for (int i = 1; i<=10; i++){
             k = i*numberTableToPrint;
-            System.out.print(numberTableToPrint + " * " + i + " = " + k);
+            System.out.print(i + " x " + numberTableToPrint + " = " + k);
         }
     }
 }
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,11 +2,31 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        String part1 = "";
+        String part2 = "";
+        String res2 = "1";
+        int k = 2;
+        if (cathetusLength != 0) {
+            for (int i = 0; i < cathetusLength - 1; i++) {
+                part1 += cathetusLength - i;
+                part2 += i + 2;
+            }
+            String bottom = part1 + "1" + part2;
+            res2 = bottom;
+            System.out.println(res2);
 
+            for (int i = 0; i < cathetusLength - 1; i++) {
+                bottom = bottom.replace("" + (cathetusLength - i), " ");
+                bottom = bottom.stripTrailing();
+
+                res2 = bottom + "\n" + res2;
+                System.out.println(res2);
+            }
+        }
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(0);
     }
 }
 //      123456789
