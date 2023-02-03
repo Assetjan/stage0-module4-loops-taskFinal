cae94ce1e0d048499258a25c96868641d7a3681a
Asset <assetziyabek@gmail.com>
2023-02-03 10:56:34 +0600

    done

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,26 +2,16 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
-        String part1 = "";
-        String part2 = "";
-        String res2 = "1";
-        int k = 2;
-        if (cathetusLength != 0) {
-            for (int i = 0; i < cathetusLength - 1; i++) {
-                part1 += cathetusLength - i;
-                part2 += i + 2;
+        String res = "1";
+        int h = 2;
+        for (int i = cathetusLength; i > 0; i--){
+            for (int j = 0; j < i-1; j++){
+                System.out.print(" ");
             }
-            String bottom = part1 + "1" + part2;
-            res2 = bottom;
-
-            for (int i = 0; i < cathetusLength - 1; i++) {
-                bottom = bottom.replace("" + (cathetusLength - i), " ");
-                bottom = bottom.stripTrailing();
-
-                res2 = bottom + "\n" + res2;
-
-            }
-            System.out.println(res2);
+            System.out.print(res);
+            System.out.println();
+            res = h + res + h;
+            h++;
         }
     }
 
